d991f9c1aa3784a51cfe7cb4617dfc08a5318c22
trsh <trsh@debian>
2020-08-02 18:15:02 -0300

    Modificacion

---
+++ README.md
@@ -0,0 +1,3 @@
+Distintos codigos de ShellScript que me fueron utiles.
+
+History: Ordena, elimina repeticiones y guarda el historial de comandos en un archivo history.log
--- history.sh
+++ history.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
-cat .bash_history | grep -v "cd" | grep -v "ls" | sort | uniq >> history.log
+#!/bin/sh
+#Ordena limpia y guarda el historial de comandos en un archivo
+cat ~/.bash_history | grep -v "cd" | grep -v "ls" | sort | uniq >> history.log
 sort history.log > history1
 uniq history1 > history.log
 rm history1
